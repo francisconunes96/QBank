5dc8cc6e4066a1dc72d15cd59cc6abd6f9a01d20
Bruno Coelho <coelho1_bruno@hotmail.com>
2019-10-28 10:26:08 -0300

    Configurações iniciais
    Co-Authored-By: francisconunes96 <francisconunes96@users.noreply.github.com>
    Co-Authored-By: alvaroaugustozago <alvaroaugustozago@users.noreply.github.com>

---
+++ src/main/java/com/totvs/tj/tcc/Application.java
@@ -0,0 +1,13 @@
+package com.totvs.tj.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
